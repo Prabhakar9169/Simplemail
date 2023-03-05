a527c561e4455541e3c276363ce92371aafed44d
gmanu0077 <66879853+gmanu0077@users.noreply.github.com>
2023-03-05 23:48:57 +0530

    nev

--- src/Navbar/Nav1.jsx
+++ src/Navbar/Nav1.jsx
@@ -24,7 +24,10 @@ function Nav1() {
                 </label>
               </div> */}
 
-              {/* <button className='btn2 btn-md btn-outline-primary'>Logout</button> */}
+              <button className='btn2 btn-md btn-outline-primary' onClick={()=>{
+                 window.open("https://mail.google.com/mail/")
+              
+              }}>Open Gmail</button>
 
               {/* <div className="nav-links">
                 <button className='tut' onClick={tut}> Back to Tutorial</button>
